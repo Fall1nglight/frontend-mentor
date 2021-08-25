40f517183c3147e6a242ac8336f43be5016b218f
Fall1nglight <keruloszabolcs1@gmail.com>
2021-08-25 17:56:15 +0200

    Added banner, background and dark/light-mode

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,26 +1,107 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container-sm p-5 mt-5">
+    <div class="row justify-content-center">
+      <div class="heading col-sm-5 col-md-4 col-lg-3">
+        <h1>Todo</h1>
+      </div>
+      <div class="toggler-icon col-sm-5 col-md-4 col-lg-3 text-end">
+        <button class="btn">
+          <img
+            :src="togglerIcon"
+            :alt="togglerIconCaption"
+            :hover="togglerIconCaption"
+            @click="toggleDarkMode"
+          />
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { computed, ref } from '@vue/reactivity';
+import { watch } from '@vue/runtime-core';
+import iconSun from './assets/icon-sun.svg';
+import iconMoon from './assets/icon-moon.svg';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  setup() {
+    //refs
+    const darkMode = ref(true);
+
+    //watches
+    watch(darkMode, () => {
+      if (darkMode.value)
+        return (document.body.classList.value = 'background-dark');
+      document.body.classList.value = 'background-light';
+    });
+
+    //computed properties
+    const togglerIcon = computed(() => (darkMode.value ? iconSun : iconMoon));
+
+    const togglerIconCaption = computed(() =>
+      darkMode.value ? 'Toggle light mode' : 'Toggle dark mode'
+    );
+
+    //functions
+    const toggleDarkMode = () => {
+      darkMode.value = !darkMode.value;
+    };
+    return {
+      darkMode,
+      togglerIcon,
+      togglerIconCaption,
+      toggleDarkMode,
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  font-family: 'Josefin Sans', sans-serif;
+}
+
+/* Background */
+.background-dark {
+  background: url('./assets/bg-mobile-dark.jpg') hsl(235, 21%, 11%);
+  background-repeat: no-repeat;
+  background-position: top center;
+  background-size: cover;
+}
+
+.background-light {
+  background: url('./assets/bg-mobile-light.jpg') hsl(0, 0%, 98%);
+  background-repeat: no-repeat;
+  background-position: top center;
+  background-size: cover;
+}
+
+@media (min-width: 576px) {
+  .background-dark {
+    background: url('./assets/bg-desktop-dark.jpg') hsl(235, 21%, 11%);
+    background-repeat: no-repeat;
+    background-position: top center;
+    background-size: cover;
+  }
+
+  .background-light {
+    background: url('./assets/bg-desktop-light.jpg') hsl(0, 0%, 98%);
+    background-repeat: no-repeat;
+    background-position: top center;
+    background-size: cover;
+  }
+}
+
+/* Heading */
+.heading {
+  text-align: start;
+}
+
+.heading h1 {
+  text-transform: uppercase;
+  font-weight: 700;
+  color: white;
 }
 </style>
